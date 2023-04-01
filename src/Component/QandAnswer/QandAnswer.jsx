fbc86acad4399e690dac6f807b9f7ed7d48185cd
asad2002web <asad2002web@gmail.com>
2023-04-01 17:20:23 +0600

    third question added

--- src/Component/QandAnswer/QandAnswer.jsx
+++ src/Component/QandAnswer/QandAnswer.jsx
@@ -13,6 +13,10 @@ const QandAnswer = () => {
           <h3>How does work useState?</h3>
           <p>useState a react hook . its allows you add state functional components. also return a array wwith two values.the hook initial state value as an argument and return update state .when set function called. useState can store any type value premitive data type,and complex data type.</p>
         </div>
+        <div className="question card px-3 py-3 my-4">
+          <h3>What does useeffect do other than load data ?</h3>
+          <p>useEffect use dependency values against the from the last render and its also effect function if any one of them changed.the react useEffect Hook essentially replaces every single lifecycle function that you may run into.</p>
+        </div>
         
       </div>
     </div>
